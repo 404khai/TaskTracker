4bc5c4cfc9f424a64b6cd8304ba493d38d6416e9
Khai <omajeneoghenefega11@gmail.com>
2026-05-26 15:27:00 -0700

    feat: basic data types

--- SwiftBasics.swift
+++ SwiftBasics.swift
@@ -7,6 +7,22 @@
 
 import Playgrounds
 
-#Playground{
+#Playground {
+    var playerName = "Alex"
+    let maxScore = 100
     
+    playerName = "Jordan"
+    
+    var age: Int = 28
+    var price: Double = 19.99
+    var isActive: Bool = false
+    var message: String = "Hello, World"
+    
+    var colors: [String] = ["Red", "Blue"]
+    colors.append("Yellow")
+    
+    var user: [String: String] = [
+        "name": "Khai",
+        "Role": "Developer"
+    ]
 }
