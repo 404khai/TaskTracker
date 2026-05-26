d4434124b07e1d9cdfeb040f20be17229d5ac5b1
Khai <omajeneoghenefega11@gmail.com>
2026-05-26 15:12:47 -0700

    feat: app init

---
+++ SwiftBasics.swift
@@ -0,0 +1,12 @@
+//
+//  SwiftBasics.swift
+//  TaskTracker
+//
+//  Created by admin on 5/26/26.
+//
+
+import Playgrounds
+
+#Playground{
+    
+}
---
+++ TaskTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTrackerApp.swift
+//  TaskTracker
+//
+//  Created by admin on 5/26/26.
+//
+
+import SwiftUI
+
+@main
+struct TaskTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
